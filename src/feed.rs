38351fb8246e3b8a64c4599e5fb156cc1a76e25d
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-05-30 21:04:22 +1200

    feat(feed): use despam.io to moderate messages

--- src/feed.rs
+++ src/feed.rs
@@ -3,10 +3,10 @@ use askama::Template;
 use axum::{
     extract::State,
     http::StatusCode,
-    response::{IntoResponse, Redirect, Response},
+    response::{Html, IntoResponse, Redirect, Response},
     Form,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{pool::PoolConnection, Sqlite, SqlitePool};
 use tower_sessions::Session;
 use tracing::info;
@@ -75,6 +75,15 @@ pub async fn post_post(
         return Ok(Redirect::to("/login").into_response());
     };
 
+    if !is_kind_message(&input.content)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+    {
+        return Ok(
+            Html("<p>Watch your lanuage pal</p><a href=\"/\">I'm sorry</a>").into_response(),
+        );
+    }
+
     sqlx::query!(
         "INSERT INTO posts (user_id, content) VALUES ( ?, ? )",
         user.id,
@@ -114,4 +123,38 @@ async fn retrieve_posts_with_user_name(conn: &mut PoolConnection<Sqlite>) -> Opt
     .fetch_all(&mut **conn)
     .await
     .ok()
+}
+
+async fn is_kind_message(input: &str) -> Result<bool, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let report: ModerationReport = client
+        .post("https://despam.io/api/v1/moderate")
+        .json(&ModerationRequest { input })
+        .header("x-api-key", include_str!("despam.token"))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(report.toxic < 0.8
+        && report.indecent < 0.9
+        && report.threat < 0.6
+        && report.offensive < 0.8
+        && report.erotic < 0.6
+        && report.spam < 0.8)
+}
+
+#[derive(Serialize)]
+struct ModerationRequest<'a> {
+    input: &'a str,
+}
+
+#[derive(Deserialize, Debug)]
+struct ModerationReport {
+    pub toxic: f64,
+    pub indecent: f64,
+    pub threat: f64,
+    pub offensive: f64,
+    pub erotic: f64,
+    pub spam: f64,
 }
